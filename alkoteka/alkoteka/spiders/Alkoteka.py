b4d2d3981424a0138afffe1e49cb4565ab5ef9a6
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-04 16:56:05 +0300

    Подправил время между запросами, добавил доп логирование

--- alkoteka/alkoteka/spiders/Alkoteka.py
+++ alkoteka/alkoteka/spiders/Alkoteka.py
@@ -1,46 +1,35 @@
 import json
 import traceback
 from datetime import datetime, UTC
+from pathlib import Path
 from random import choice
 
-import requests
 from scrapy import Spider, Request, http
-from fake_useragent import UserAgent
-from urllib.parse import urlparse, urlsplit
+from scrapy.exceptions import NotConfigured
 from alkoteka.items import AlkotekaItem
+from alkoteka.spiders.AlkotekaConfig import AlkotekaConfig
+
+from fake_useragent import UserAgent
+from urllib.parse import urlparse
+
 from typing_extensions import deprecated
 
 
 # uuid city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416 - Краснодар
 
-# https://alkoteka.com/catalog/slaboalkogolnye-napitki-2
-# https://alkoteka.com/web-api/v1/product?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page=1&per_page=20&root_category_slug=slaboalkogolnye-napitki-2
-# TODO не забыть про бесплатные прокси
-
-def get_random_headers() -> dict:
-    """
-    Используется для получения динамического header в части user-agent
-    :return:
-    """
-    user_agent = UserAgent().random
-
-    return {
-        'Accept': '*/*',
-        'User-Agent': user_agent,
-        'Content-Type': 'application/json',
-        'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8,ky;q=0.7',
-    }
-
 
 class Alkoteka(Spider):
     name = 'alkoteka'
-    BASE_URL = 'https://alkoteka.com'
     STARTS_URL = (
         'https://alkoteka.com/catalog/vino',
         'https://alkoteka.com/catalog/krepkiy-alkogol',
         'https://alkoteka.com/catalog/slaboalkogolnye-napitki-2',
 
     )
+    """
+        Если не передавать путь к файлу при старте паука, то паук будет брать  url из  переменной STARTS_URL
+        
+    """
 
     ROOT_CATEGORY_SLUGS = [
         # 'vino',
@@ -54,43 +43,35 @@ class Alkoteka(Spider):
 
     allowed_domains = ["alkoteka.com"]
 
-    # ALKOTEKA_API_CATALOG = 'https://alkoteka.com/web-api/v1/product?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page={page}&per_page=20&root_category_slug={root_category_slug}'
-    ALKOTEKA_API_CATALOG = 'https://alkoteka.com/web-api/v1/product?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page={page}&per_page={per_page}&root_category_slug={root_category_slug}'
-    """
-    в meta "has_more_pages": false, - можно определить  последняя ли это страница
-    """
+    config: AlkotekaConfig = AlkotekaConfig()
 
-    ALKOTEKA_API_CITY_SEARCH = 'https://alkoteka.com/web-api/v1/city?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&search={city_name}'
-    """
-    API для поиска города, передается в параметр search
-    """
-
-    ALKOTEKA_API_ALL_CITY = 'https://alkoteka.com/web-api/v1/city?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page={page}'  # всего 3 страницы, 59 городов
-    """
-    Можно получить все доступные города, ответ по страницам. 
-    """
-
-    ALKOTEKA_API_ITEM_URL = 'https://alkoteka.com/web-api/v1/product/{item_slug}?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416'
-    """
-    API для получения данных об определенном товаре
-    """
-
-    PROXY_FILEPATH = "proxy_pool.json"
-
-    city_uuid = '4a70f9e0-46ae-11e7-83ff-00155d026416'
-    """
-    идентификатор Краснодара для API       
-    """
-
-    def __init__(self, from_file=False, file_path=None, *args, **kwargs) -> None:
+    def __init__(self, file_path=None, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        # TODO доделать возможность загрузки урлов при старте парсера
-        # if from_file:
-        #     with open(file_path, 'r', encoding='utf-8') as f:
-        #         print(f.readlines())
-        #         # type(self).STARTS_URL = list(map(str.strip(f.readlines()))
+
+        if file_path and Path(file_path).exists():
+            with open(file_path, 'r', encoding='utf-8') as f:
+                urls_from_file = map(lambda item: item.strip(), f.readlines())
+                type(self).STARTS_URL = sorted(set(urls_from_file))
+        else:
+            self.logger.error(
+                f"🚨 Файл {file_path} не существует! URL будут получены из атрибута класса STARTS_URL, проверьте что бы он был заполнен в коде.")
 
         self.proxy_pool = type(self)._get_proxies()
+
+    @staticmethod
+    def get_random_headers() -> dict:
+        """
+        Используется для получения динамического header в части user-agent
+        :return:
+        """
+        user_agent = UserAgent().random
+
+        return {
+            'Accept': '*/*',
+            'User-Agent': user_agent,
+            'Content-Type': 'application/json',
+            'Accept-Language': 'ru,en-US;q=0.9,en;q=0.8,ky;q=0.7',
+        }
 
     @classmethod
     def _get_proxies(cls):
@@ -99,7 +80,7 @@ class Alkoteka(Spider):
         :return:
         """
 
-        with open(cls.PROXY_FILEPATH) as f:
+        with open(cls.config.PROXY_FILEPATH) as f:
             proxies = json.load(f)
 
             return proxies
@@ -227,12 +208,7 @@ class Alkoteka(Spider):
         :return:
         """
         import math
-        print("=" * 10)
-        print("=" * 10)
-        print(int(datetime.now(UTC).timestamp()))
-        print("=" * 10)
-        print(filters)
-        print("=" * 10)
+
         VARIANT_CODES = {'cvet', 'obem', 'massa'}
 
         total_variants = 1
@@ -345,70 +321,113 @@ class Alkoteka(Spider):
 
         return result
 
-    def start_requests(self):
+    def _make_request(self, slug, page):
+        url = type(self).config.ALKOTEKA_API_CATALOG.format(
+            page=page,
+            per_page=type(self).config.DEFAULT_PER_PAGE,
+            root_category_slug=slug
+        )
+        payload = {
+            'city_uuid': type(self).config.city_uuid,
+            'page': page,
+            'root_category_slug': slug,
+            'per_page': type(self).config.DEFAULT_PER_PAGE,
 
+        }
+        return Request(
+            url=url,
+            headers=type(self).get_random_headers(),
+            body=json.dumps(payload),
+            callback=self.parse,
+            meta={
+                'page': page,
+                'root_category_slug': slug,
+                # 'proxy': choice(self.proxy_pool)
+            }
+
+        )
+
+    def start_requests(self):
+        if not hasattr(self, 'ROOT_CATEGORY_SLUGS'):
+            self.ROOT_CATEGORY_SLUGS = []
         if type(self).STARTS_URL:
             type(self).ROOT_CATEGORY_SLUGS.extend(
                 type(self)._parse_input_url(type(self).STARTS_URL)
             )
+        else:
+            raise NotConfigured("Не сконфигурирован атрибут класса: STARTS_URL! Нужно добавить в него URL.")
+
+        if not type(self).ROOT_CATEGORY_SLUGS:
+            raise NotConfigured(
+                "Пуст список слагов для формирования API! Проверьте STARTS_URL или указанный файл с URL.")
 
         for slug in type(self).ROOT_CATEGORY_SLUGS:
-            # for page in range(1, 10):
-            for page in range(1, 2):
-                url = type(self).ALKOTEKA_API_CATALOG.format(
-                    page=page,
-                    per_page=200,
-                    root_category_slug=slug
-                )
-                payload = {
-                    'city_uuid': type(self).city_uuid,
-                    'page': page,
-                    'root_category_slug': slug,
-                    'per_page': 200,
-
-                }
-                yield Request(
-                    url=url,
-                    headers=get_random_headers(),
-                    body=json.dumps(payload),
-                    callback=self.parse,
-                    meta={
-                        'page': page,
-                        'root_category_slug': slug,
-                        # 'proxy': choice(self.proxy_pool)
-                    }
-
-                )
+            yield self._make_request(slug, page=1)
 
     def parse(self, response: http.JsonResponse) -> dict:
-        """Обработка JSON-ответа общая"""
+        """
+            Обрабатывает все найденные слаги товаров в start_requests
+            После вызывает parse_items для получения данных о товаре.
+        :param response:
+        :return:
+        """
+
+        def get_total_pages(total, per_page):
+            pages = divmod(total, per_page)
+            if pages[1] == 0:
+                return pages[0]
+
+            return pages[0] + 1
+
         try:
             data = response.json()
+            current_page = data.get("meta").get("current_page")
+            has_more_pages = data.get("meta").get("has_more_pages", False)
+            # total_items = _a  if (_a:=response.meta.get("total_items")) else data.get("meta").get("total", 0)
+            total_items = data.get("meta").get("total", 0)
 
-            for item in data.get('results', []):
+            total_pages = get_total_pages(total_items, type(self).config.DEFAULT_PER_PAGE)
+            for index, item in enumerate(data.get('results', []), 1):
                 if (item_slug := item.get("slug")):
-                    url = type(self).ALKOTEKA_API_ITEM_URL.format(item_slug=item_slug)
+                    url = type(self).config.ALKOTEKA_API_ITEM_URL.format(item_slug=item_slug)
                     payload = {
-                        'city_uuid': type(self).city_uuid,
+                        'city_uuid': type(self).config.city_uuid,
                     }
                     yield Request(
                         url=url,
-                        headers=get_random_headers(),
+                        headers=type(self).get_random_headers(),
                         body=json.dumps(payload),
                         callback=self.parse_items,
                         meta={
                             'item_slug': item_slug,
                             'product_url': item.get("product_url"),
+                            'root_category_slug': response.meta.get("root_category_slug"),
+                            'page': current_page,
+                            'total_pages': total_pages,
+                            'index': index
+
                             # 'proxy': choice(self.proxy_pool)
                         }
 
                     )
 
-                # break
+            if has_more_pages:
+                current_page += 1
+                yield self._make_request(response.meta.get("root_category_slug"), page=current_page)
+                self.logger.warning(
+                    f"🌐 Веб-страница {current_page}/{total_pages} обработана. Категория: '{response.meta.get("root_category_slug")}'")
+
+
+            else:
+                self.logger.warning(
+                    f"🔚 Достигнута последняя страница для категории '{response.meta.get("root_category_slug")}'. Всего страниц: {current_page}")
+
+
+
 
 
         except Exception as e:
-            self.logger.error(f"Failed to parse JSON on {response.url}: {e}")
+            self.logger.error(f"🚨 [ PARSE ] Ошибка при парсинге JSON {response.url}: {e}")
             return
 
     def parse_items(self, response: http.JsonResponse) -> dict:
@@ -441,23 +460,34 @@ class Alkoteka(Spider):
                     "count": data.get("quantity_total", 0),
                 }
 
+                alkoteka_item["assets"] = {
+                    "main_image": data.get("image_url"),  # Ссылка на основное изображение товара.
+                    "set_images": [],  # Список ссылок на все изображения товара - НА САЙТЕ НЕ ПОПАЛИСЬ.
+                    "view360": [],  # Список ссылок на изображения в формате 360 - НА САЙТЕ НЕ ПОПАЛИСЬ.
+                    "video": []  # Список ссылок на видео/видеообложки товара - НА САЙТЕ НЕ ПОПАЛИСЬ.
+                },
+
                 alkoteka_item["metadata"] = {
                     "__description": type(self)._get_description(data.get("text_blocks")),
                     "Артикул": data.get("vendor_code"),
                     **type(self)._get_all_characteristics(data.get("description_blocks"))
                 }
                 alkoteka_item["variants"] = type(self)._get_variants_count(data.get("description_blocks"))
-                self.logger.info(f"{alkoteka_item}")
+
+                self.logger.info(f"ℹ️️ {alkoteka_item}")
+
+                self.logger.warning(
+                    f"✅ #{response.meta.get("index")}_PAGE#({response.meta.get("page")}/{response.meta.get("total_pages")})_SLUG#{response.meta.get("root_category_slug")} URL: {response.meta.get('product_url')}")
                 yield alkoteka_item
 
 
 
         except Exception as e:
             self.logger.error(
-                f"[ PARSE_ITEMS ] Failed to parse JSON on {response.url}: {e.args} {traceback.format_exc()}")
-            self.logger.error(f"[ PARSE_ITEMS ] {'=' * 20}")
-            self.logger.error(f"[ PARSE_ITEMS ] {data.get('description_blocks')}")
-            self.logger.error(f"[ PARSE_ITEMS ] {'=' * 20}")
+                f"🚨 [ PARSE_ITEMS ] Failed to parse JSON on {response.url}: {e.args} {traceback.format_exc()}")
+            self.logger.debug(f"🚨 [ PARSE_ITEMS ] {'=' * 20}")
+            self.logger.debug(f"🚨 [ PARSE_ITEMS ] {data.get('description_blocks')}")
+            self.logger.debug(f"🚨 [ PARSE_ITEMS ] {'=' * 20}")
             return
 
 
