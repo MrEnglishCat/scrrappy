a40c5dcdce889744fcf544a715c1f15790eec8bf
Mr. EnglishCat <mr.englishcat@gmail.com>
2025-09-04 16:02:59 +0300

    конфигурация API вынес в отдельный класс

---
+++ alkoteka/alkoteka/spiders/AlkotekaConfig.py
@@ -0,0 +1,41 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class AlkotekaConfig:
+    BASE_URL = 'https://alkoteka.com'
+
+    ALKOTEKA_API_CATALOG: str = 'https://alkoteka.com/web-api/v1/product?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page={page}&per_page={per_page}&root_category_slug={root_category_slug}'
+    """
+    в meta "has_more_pages": false, - можно определить  последняя ли это страница
+    Используется для получения списка слагов на товары. Лимиты выставляются передавая параметр per_page, page
+    """
+
+    ALKOTEKA_API_ITEM_URL: str = 'https://alkoteka.com/web-api/v1/product/{item_slug}?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416'
+    """
+    API для получения данных об определенном товаре
+    """
+
+    ALKOTEKA_API_CITY_SEARCH: str = 'https://alkoteka.com/web-api/v1/city?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&search={city_name}'
+    """
+    API для поиска города, передается в параметр search
+    
+    НЕ ИСПОЛЬЗУЕТСЯ, НО ОНА ЕСТЬ
+    """
+
+    ALKOTEKA_API_ALL_CITY: str = 'https://alkoteka.com/web-api/v1/city?city_uuid=4a70f9e0-46ae-11e7-83ff-00155d026416&page={page}'  # всего 3 страницы, 59 городов
+    """
+    Можно получить все доступные города, ответ по страницам. 
+    
+    НЕ ИСПОЛЬЗУЕТСЯ, НО ОНА ЕСТЬ
+    """
+
+    PROXY_FILEPATH: str = "proxy_pool.json"
+
+    city_uuid: str = '4a70f9e0-46ae-11e7-83ff-00155d026416'
+    """
+    идентификатор Краснодара для API       
+    """
+
+
+    DEFAULT_PER_PAGE = 200
